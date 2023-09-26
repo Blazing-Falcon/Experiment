034e430d97dac5b4709811dd802e8254e2833915
Farhan Kurnia <82351463+Blazing-Falcon@users.noreply.github.com>
2023-09-26 11:55:44 +0000

    add modul_2/kegiatan1 solution

---
+++ modul_2/kegiatan1/kegiatan1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+    float mat;
+    float fis;
+    float bio;
+    float hasil;
+
+    printf("Masukkan nilai Matematika: ");
+    scanf("%f", &mat);
+    printf("Masukkan nilai Fisika: ");
+    scanf("%f", &fis);
+    printf("Masukkan nilai Biologi: ");
+    scanf("%f", &bio);
+    hasil = (mat + fis + bio)/3;
+    printf("Nilai Rata-rata: %.2f\n", hasil);
+
+}
