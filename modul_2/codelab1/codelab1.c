6b68e10a9689aabff0710ed3598d785e6476594c
Farhan Kurnia <82351463+Blazing-Falcon@users.noreply.github.com>
2023-10-09 10:09:56 +0000

    revise modul_2/codelab1 solution

--- modul_2/codelab1/codelab1.c
+++ modul_2/codelab1/codelab1.c
@@ -5,9 +5,7 @@ int main(){
     float sapiJantan2;
     float sapiBetina1;
     float sapiBetina2;
-    float kambingJantan1;
-    float kambingJantan2;
-    float kambingBetina1;
+    const float kambing1=120, kambing2=85.8, kambing3=23;
 
     printf("Masukkan berat sapi 1: ");
     scanf("%f", &sapiJantan1);
@@ -20,17 +18,7 @@ int main(){
     
     printf("Masukkan berat sapi 4: ");
     scanf("%f", &sapiBetina2);
-    
-    printf("Masukkan berat kambing 1: ");
-    scanf("%f", &kambingJantan1);
-    
-    printf("Masukkan berat kambing 2: ");
-    scanf("%f", &kambingJantan2);
-    
-    printf("Masukkan berat kambing 3: ");
-    scanf("%f", &kambingBetina1);
-    
 
     printf("\nHasil Berat Sapi: %.2f\n", sapiJantan1 + sapiJantan2 + sapiBetina1 + sapiBetina2);
-    printf("Hasil Berat Kambing: %2.f\n", kambingJantan1 + kambingJantan2 + kambingBetina1);
+    printf("Hasil Berat Kambing: %.2f\n", kambing1 + kambing2 + kambing3);
 }
